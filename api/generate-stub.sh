27bcf0ffb7c0685a854efaec6bcfeb62fb0e8367
Ryan Li <hustrlee@gmail.com>
2021-06-03 17:21:43 +0800

    ignore

--- api/generate-stub.sh
+++ api/generate-stub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:latest-release generate \
   -i /local/bill-segmentation-api.yaml \
   -g python-flask \
   -o /local/python-flask \
